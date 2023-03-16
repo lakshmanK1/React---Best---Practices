0950903972f5cba9b11c0bec5dc7a4daf76bb05a
lakshmanK1 <laxmankamisetty1@gmail.com>
2023-03-16 12:21:40 +0530

    imported custom hook, which contains counter component logic

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Counter from '../Counter/Counter'
+import useCounter from '../useCounter/useCounter'
 
 function Home() {
+    const {currentVal, increment, decrement} = useCounter();
   return (
     <div>
-        <Counter/>
+        <Counter value={currentVal} increment={increment} decrement={decrement}/>
     </div>
   )
 }
