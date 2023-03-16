ccf92f33723e4bc09f03520cc5a6bac57082b1e3
lakshmanK1 <laxmankamisetty1@gmail.com>
2023-03-16 11:55:16 +0530

    created separate folders for specific files

---
+++ src/Components/Counter/Counter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Counter() {
+  return (
+    <div>
+        <button> + </button>
+        content 
+        <button> - </button>
+    </div>
+  )
+}
+
+export default Counter
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Counter from '../Counter/Counter'
+
+function Home() {
+  return (
+    <div>
+        <Counter/>
+    </div>
+  )
+}
+
+export default Home
